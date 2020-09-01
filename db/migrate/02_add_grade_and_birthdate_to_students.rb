5f55538549255e436b659a2eabf916a92327b7e2
Austin Cummings <aac729@live.com>
2020-09-01 17:22:05 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthdateToStudents
+class AddGradeAndBirthdateToStudents < ActiveRecord::Base
   def change
     add_column :students, :grade, :integer 
     add_column :students, :birthdate, :string
